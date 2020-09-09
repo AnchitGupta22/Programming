dabe776c0f9c43253c02375edab44cee0ac88c6b
Anchit <53094342+AnchitGupta22@users.noreply.github.com>
2020-09-09 16:04:23 +0530

    visualizes each step of sorting

---
+++ Sorting/graph.py
@@ -0,0 +1,13 @@
+# importing libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# function to visualize the step by step state of the array
+def draw_graph(x, y, data, title):
+    sns.set(style="darkgrid", rc={'figure.figsize':(5, 3)})
+    
+    ax = sns.barplot(x=x, y=y, data=data)
+    ax.set_title(title, fontsize=20)
+    
+    plt.show()
